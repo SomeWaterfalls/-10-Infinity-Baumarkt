9d7a956d909bcccddfdaf44ef2e08af349fdb188
SomeWaterfalls <lina009.nos@gmx.de>
2016-12-09 09:53:51 +0100

    Möp

---
+++ src/Model/Customer.java
@@ -0,0 +1,14 @@
+package Model;
+
+/**
+ * Created by 204g11 on 09.12.2016.
+ */
+public class Customer {
+    private ShoppingCart cart;
+    private double budget;
+
+    public Customer(){
+        cart = new ShoppingCart();
+
+    }
+}
---
+++ src/Model/Register.java
@@ -0,0 +1,13 @@
+package Model;
+
+/**
+ * Created by 204g11 on 09.12.2016.
+ */
+public class Register {
+    private CashQueue queue;
+
+    public Register(){
+        queue = new CashQueue();
+
+    }
+}
